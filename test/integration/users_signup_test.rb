6f36aa3131d4106f252874cde653d238b9e8d0f3
ZeitungM <http418im.a.teapot@gmail.com>
2020-03-25 15:18:41 +0000

    エラーメッセージの処理を書いた(7.3.3)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,4 +4,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  test "invalid signup information" do
+    get signup_path # get でユーザ登録ページへアクセス
+    assert_no_difference 'User.count' do # assert_no_difference ブロックの実行前後で User.count が変化しないことを確認
+      # post メソッドでリクエストを送信 (User.new に使うデータをハッシュ params[:user] にまとめる )
+      post signup_path, params:
+      {
+        user:
+          {
+            name: "",
+            email: "user@invalid",
+            password: "foo",
+            password_confirmation: "bar"
+          }
+      }
+    end
+    
+    assert_template 'users/new'
+    assert_select   'form[action="/signup"]'
+    assert_select   'div#error_explanation'
+    assert_select   'div.field_with_errors'
+  end
+  
 end
