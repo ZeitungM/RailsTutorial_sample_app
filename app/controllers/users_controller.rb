efc7e8377e396d1f8e4b7c9fc66db58611adc32b
ZeitungM <http418im.a.teapot@gmail.com>
2020-05-13 15:36:23 +0000

    無効な情報送信時に再度editをレンダリングしてエラーメッセージを出すようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,18 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params) # Strong Parameter で マスアサインメント脆弱性防止
+      # 更新成功
+    else
+      render 'edit'
+    end
+  end
   
   private
     def user_params
