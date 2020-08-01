430d8c90fa2829b8ad6bebd37ca3baaa77ab5f31
ZeitungM <http418im.a.teapot@gmail.com>
2020-08-01 08:44:00 +0000

    ログイン時にhomeでmicropostを投稿するフォームを作った(13.3.2)

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,21 @@ class MicropostsController < ApplicationController
   before_action :is_logged_in_user, only: [ :create, :destroy]
   
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
   
   def destroy
   end
+  
+  private
+    def micropost_params
+      # content のみ変更を加えることを許可する
+      params.require(:micropost).permit(:content)
+    end
 end
