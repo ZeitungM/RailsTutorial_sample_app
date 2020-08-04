e3bef167c06ad9c72fb10b468ccb6f22ce4d67e6
ZeitungM <http418im.a.teapot@gmail.com>
2020-08-04 15:00:51 +0000

    Micropost削除機能を作った(13.3.4)

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :is_logged_in_user, only: [ :create, :destroy]
+  before_action :correct_user,      only: :destroy
   
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,11 +14,21 @@ class MicropostsController < ApplicationController
   end
   
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    # request.referrer で一つ前の URL (DELETE リクエストが発行されたページ) にリダイレクトする
+    redirect_to request.referrer || root_url
+    # redirect_back(fallback_location: root_url) # rails 5 からはこのような書き方でも可
   end
   
   private
     def micropost_params
       # content のみ変更を加えることを許可する
       params.require(:micropost).permit(:content)
+    end
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
