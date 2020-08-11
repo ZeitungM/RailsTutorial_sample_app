fe5c7c9fdbf4c21fda8d29fe57a9a86ecacde247
ZeitungM <http418im.a.teapot@gmail.com>
2020-08-11 12:11:06 +0000

    フォローに関するメソッドとそのテストを書いた(14.1.4)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
@@ -83,6 +84,18 @@ class User < ApplicationRecord
   
   def password_reset_exired?
     reset_sent_at < 2.hours.ago # メールを送った時刻が 2 時間前より早かったら true
+  end
+  
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
+  def follow(other_user)
+    following << other_user
+  end
+  
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
   end
   
   private
